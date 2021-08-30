1961c1c81d935b6c5b859bedc9f0dcd8032d9bb9
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-08-30 16:07:34 +0700

    Update install-ws.sh

--- websocket/install-ws.sh
+++ websocket/install-ws.sh
@@ -8,7 +8,7 @@ cd
 wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/openssh-socket.py
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/ws-stunnel && chmod +x /usr/local/bin/ws-stunnel
-#wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/fardinzaga/websocketssh/master/wsssl/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
+#wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
 #izin permision
 chmod +x /usr/local/bin/ws-openssh
