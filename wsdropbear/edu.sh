f905823f26f96a1f1c47835cc6673a77aea63339
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-08-31 20:04:52 +0700

    Create edu.sh

---
+++ wsdropbear/edu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Proxy For Edukasi, Imclass & gamemax
+# ==============================
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/edu-proxy linkkalian/proxy.py
+chmod +x /usr/local/bin/edu-proxy
+
+# Installing Service
+cat > /etc/systemd/system/edu-proxy.service << END
+[Unit]
+Description=Python Edu Proxy By Fauzanvpn
+Documentation=admin@geolstore.net
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 2086
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable edu-proxy
+systemctl restart edu-proxy
+
+clear
+echo -e Fauzanvpn
