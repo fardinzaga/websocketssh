08e5592fe274b8b8a969dde59f64a13b0d610908
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-01 16:29:44 +0700

    Update usernew.sh

--- menu/usernew.sh
+++ menu/usernew.sh
@@ -19,7 +19,9 @@ read -p "Expired (hari): " masaaktif
 
 IP=$(wget -qO- icanhazip.com);
 domain=$(cat /etc/v2ray/domain)
+ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
+portws="$(cat ~/log-install.txt | grep -w "WebSocket" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 sleep 1
@@ -47,10 +49,10 @@ echo -e "Host           : $IP"
 echo -e "Host           : $domain"
 echo -e "OpenSSH        : 22"
 echo -e "Dropbear       : 109, 143"
-echo -e "SSL/TLS        : 443, 777"
+echo -e "SSL/TLS        :$ssl"
 echo -e "Port Squid     :$sqd"
-echo -e "Port WebSocket : 2082"
-echo -e "Port Ws All    : 2086 , 2092"
+echo -e "Port WebSocket :$portws"
+echo -e "Port Ws All    : 2086 , 2090"
 echo -e "OpenVPN        : TCP $ovpn http://$IP:81/client-tcp-$ovpn.ovpn"
 echo -e "OpenVPN        : UDP $ovpn2 http://$IP:81/client-udp-$ovpn2.ovpn"
 echo -e "OpenVPN        : SSL 442 http://$IP:81/client-tcp-ssl.ovpn"
