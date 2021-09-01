97b61c55e6f519abf87ac4c3333131e34a18b642
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-01 18:37:06 +0700

    Update usernew.sh

--- menu/usernew.sh
+++ menu/usernew.sh
@@ -22,6 +22,7 @@ domain=$(cat /etc/v2ray/domain)
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 portws="$(cat ~/log-install.txt | grep -w "WebSocket" | cut -d: -f2)"
+wsport="$(cat ~/log-install.txt | grep -w "WebSocket All Port" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 sleep 1
