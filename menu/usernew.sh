3d5746f7425c5b3997b740d17868d6ef234c9ddc
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-01 13:04:41 +0700

    Update usernew.sh

--- menu/usernew.sh
+++ menu/usernew.sh
@@ -21,6 +21,7 @@ IP=$(wget -qO- icanhazip.com);
 domain=$(cat /etc/v2ray/domain)
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
+portws="$(cat ~/log-install.txt | grep -w "WebSocket" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 sleep 1
@@ -50,8 +51,8 @@ echo -e "OpenSSH        : 22"
 echo -e "Dropbear       : 109, 143"
 echo -e "SSL/TLS        :$ssl"
 echo -e "Port Squid     :$sqd"
-echo -e "Port WebSocket : 2082"
-echo -e "Port Ws All    : 2086 , 2096"
+echo -e "Port WebSocket :$portws"
+echo -e "Port Ws All    : 2086 , 2081"
 echo -e "OpenVPN        : TCP $ovpn http://$IP:81/client-tcp-$ovpn.ovpn"
 echo -e "OpenVPN        : UDP $ovpn2 http://$IP:81/client-udp-$ovpn2.ovpn"
 echo -e "OpenVPN        : SSL 442 http://$IP:81/client-tcp-ssl.ovpn"
