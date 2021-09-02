cf0f1e7506b3dec30dd81072f0e62999ed17cfa7
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-02 08:05:47 +0700

    Update trial.sh

--- menu/trial.sh
+++ menu/trial.sh
@@ -15,6 +15,7 @@ fi
 clear
 IP=$(wget -qO- icanhazip.com);
 domain=$(cat /etc/v2ray/domain)
+ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
@@ -44,7 +45,7 @@ echo -e "Host           : $IP"
 echo -e "Host           : $domain"
 echo -e "OpenSSH        : 22"
 echo -e "Dropbear       : 109, 143"
-echo -e "SSL/TLS        : 443, 777"
+echo -e "SSL/TLS        :$ssl"
 echo -e "Port Squid     :$sqd"
 echo -e "Port WebSocket : 2082"
 echo -e "Port Ws All    : 2086"
