a323aa05439defa58b150a4e9fa0df97dcf4319b
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-13 13:29:32 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -82,7 +82,7 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 # install
 apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
 echo "clear" >> .profile
-echo "neofetch" >> .profile
+echo "echo ==========================" >> .profile
 echo "echo Selamat Datang Fauzanvpn !" >> .profile
 echo "echo Ketik menu untuk melihat list" >> .profile
 echo "echo VPSmu Terinstall AutoScript by Fauzanvpn" >> .profile
