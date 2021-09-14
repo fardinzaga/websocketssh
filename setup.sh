eb30c8fcbf17038141fc1c7acfb66adb10e605f2
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-14 14:02:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -64,7 +64,7 @@ echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
 echo "   - Stunnel4                : 443, 777"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
-echo "   - Websocket               : 2087"  | tee -a log-install.txt
+echo "   - Websocket               : 2075"  | tee -a log-install.txt
 echo "   - WebSocket Openssh       : 2052 , 2082"  | tee -a log-install.txt
 echo "   - WebSocket Dropbear      : 8880 , 2086"  | tee -a log-install.txt
 echo "   - Websocket Ssl           : 443"  | tee -a log-install.txt
