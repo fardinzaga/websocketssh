7ac1b1b9e00dbfebd120be9fda4701931c999a82
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-14 13:55:56 +0700

    Update joung-ws.py

--- proxy/joung-ws.py
+++ proxy/joung-ws.py
@@ -6,14 +6,14 @@ LISTENING_ADDR = '0.0.0.0'
 if sys.argv[1:]:
   LISTENING_PORT = sys.argv[1]
 else:
-  LISTENING_PORT = 2087
+  LISTENING_PORT = 2052
 # Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:300'
+DEFAULT_HOST = '127.0.0.1:4000'
 RESPONSE = 'HTTP/1.1 101 <font color="red">Switching Protocol</font>\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
