fa2ce670b9d7eb12336f1351933d21d13b1e522c
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-14 19:57:17 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -204,6 +204,41 @@ systemctl daemon-reload
 systemctl enable edu-proxy
 systemctl restart edu-proxy
 
+#!/bin/bash
+# Proxy For Edukasi, Imclass & gamemax
+# ==============================
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/edu-proxy linkkalian/proxy.py
+chmod +x /usr/local/bin/edu-proxy
+
+# Installing Service
+cat > /etc/systemd/system/edu-proxy.service << END
+[Unit]
+Description=Python Edu Proxy By Fauzanvpn
+Documentation=admin@Fauzanvpn
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 80
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable edu-proxy
+systemctl restart edu-proxy
+
+clear
+echo -e Fauzanvpn
+
 #OpenVPN
 #wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/vpn/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
 
