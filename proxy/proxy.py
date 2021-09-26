74dfdd8d02b25ac05e33edc6357f08913e4af2dc
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-26 12:16:58 +0700

    Update proxy.py

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -11,7 +11,7 @@ PASS = ''
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:5000'
+DEFAULT_HOST = '127.0.0.1:276'
 RESPONSE = 'HTTP/1.1 101 <font color="red">Setup By Fauzan-vpn</font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
