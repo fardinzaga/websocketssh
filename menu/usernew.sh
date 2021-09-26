bf9b9f29abff1e0aa968774c1599bd2bdd83ac94
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-26 21:12:51 +0700

    Update usernew.sh

--- menu/usernew.sh
+++ menu/usernew.sh
@@ -63,5 +63,6 @@ echo -e "Expired On       : $exp"
 echo -e "================================="
 echo -e "Payload SSH Websocket"                                                          
 echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "GET wss://masukan bug [protocol][crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]
 echo -e "================================="
 echo -e "Script Mod By Fauzanvpn"
