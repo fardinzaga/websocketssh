34a24596b1871292f3a0aa1e2ad39452475a6843
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-28 07:36:25 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -123,9 +123,19 @@ apt -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=44/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 77 -p 2000 -p 4000 -p 276 -p 187 -p 143 -p 109 "/g' /etc/default/dropbear
+
+# update dropbear 2019
+wget https://matt.ucc.asn.au/dropbear/releases/dropbear-2019.78.tar.bz2
+bzip2 -cd dropbear-2019.78.tar.bz2 | tar xvf -
+cd dropbear-2019.78
+./configure
+make && make install
+mv /usr/sbin/dropbear /usr/sbin/dropbear1
+ln /usr/local/sbin/dropbear /usr/sbin/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/dropbear restart
+cd
 
 # install squid
 cd
