2705f3a8920de43363393d764ba478a58d660a0d
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-09-30 20:05:38 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -289,8 +289,8 @@ echo "0 8 * * * root clear-log && reboot" >> /etc/crontab
 echo "0 0 * * * root xp" >> /etc/crontab
 
 #auto reboot server2
-echo "2 0 * * * root clear-log && reboot" >> /etc/crontab
-echo "0 0 * * * root xp" >> /etc/crontab
+echo "2 0 * * * root clear-log && reboot" >> /etc/otomatis
+echo "0 0 * * * root xp" >> /etc/otomatis
 
 # remove unnecessary files
 cd
