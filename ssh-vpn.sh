3b7f8855aac6f0cfd7db457f4344b2c03673c947
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-01 18:10:53 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -294,11 +294,9 @@ wget -O /usr/local/bin/userdelexpired "https://raw.githubusercontent.com/fardinz
 
 #auto reboot server
 echo "0 8 * * * root clear-log && reboot" >> /etc/crontab
-echo "0 0 * * * root xp" >> /etc/crontab
 
 #auto reboot server
 echo "0 20 * * * root clear-log && reboot" >> /etc/crontab
-echo "0 0 * * * root xp" >> /etc/crontab
 
 # remove unnecessary files
 cd
