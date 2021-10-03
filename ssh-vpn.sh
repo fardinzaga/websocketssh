4a9fc74dc18210dd0e3bdb89f17b3a7777d7b87f
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-03 10:19:52 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -209,7 +209,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 80
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 2094
 Restart=on-failure
 
 [Install]
