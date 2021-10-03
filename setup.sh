6c58827b1021273b94638b8ee96594fb74cfd37c
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-03 09:41:17 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,9 +38,9 @@ echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
 echo "   - Stunnel4                : 444, 777"  | tee -a log-install.txt
-echo "   - Ws Ssl                  : 443, 445"  | tee -a log-install.txt
+echo "   - Ws Ssl                  : 443"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
-echo "   - Websocket               : 80"  | tee -a log-install.txt
+echo "   - Websocket               : 80, 2095"  | tee -a log-install.txt
 echo "   - WebSocket Openssh       : 2052, 2082"  | tee -a log-install.txt
 echo "   - WebSocket Dropbear      : 8880, 2086"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
