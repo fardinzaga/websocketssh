db066e7bac256f5f718bd32b578487cd62f2bb58
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-03 09:06:01 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -270,6 +270,10 @@ wget -O ceklim "https://raw.githubusercontent.com/fardinzaga/websocketssh/master
 wget -O tendang "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/menu/tendang.sh"
 wget -O wbmn "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/menu/webmin.sh"
 wget -O kernel-updt "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/menu/karnel-update.sh"
+
+echo "0 8 * * * root clear-log && reboot" >> /etc/crontab
+echo "0 20 * * * root clear-log && reboot" >> /etc/crontab
+
 chmod +x add-host 
 chmod +x menu
 chmod +x usernew
@@ -295,9 +299,6 @@ wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/websocket/
 # Delete Acount SSH Expired
 echo "================  Auto deleted Account Expired ======================"
 wget -O /usr/local/bin/userdelexpired "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/userdelexpired" && chmod +x /usr/local/bin/userdelexpired
-
-echo "0 8 * * * root clear-log && reboot" >> /etc/crontab
-echo "0 20 * * * root clear-log && reboot" >> /etc/crontab
 
 # remove unnecessary files
 cd
