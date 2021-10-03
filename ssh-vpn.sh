067938109d563178d2232dec6248b427526e9fb8
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-03 09:45:43 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -309,6 +309,7 @@ apt autoremove -y
 cd
 chown -R www-data:www-data /home/vps/public_html
 /etc/init.d/nginx restart
+/etc/init.d/openvpn restart
 /etc/init.d/cron restart
 /etc/init.d/ssh restart
 /etc/init.d/dropbear restart
