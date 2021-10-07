6d01d29b5aecb279c72a71790ca7df5c0356a87c
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-07 18:29:59 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,76 +1,56 @@
-#!/bin/bash
-if [ "${EUID}" -ne 0 ]; then
-  echo "You need to run this script as root"
-  exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-  echo "OpenVZ is not supported"
-  exit 1
-fi
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-if [ -f "/etc/v2ray/domain" ]; then
-echo "Script Already Installed"
-exit 0
-fi
-mkdir /var/lib/premium-script;
-mkdir /etc/v2ray;
-echo "IP=" >> /var/lib/premium-script/ipvps.conf
-echo "$host" >> /etc/v2ray/domain
-wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/cf/cf.sh && chmod +x cf.sh && ./cf.sh
-#install ssh ovpn
-wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn.sh ./ssh-vpn.sh
-
-rm -f /root/ssh-vpn.sh
-history -c
-echo "1.2" > /home/ver
-clear
-echo " "
-echo "Installation has been completed!!"
-echo " "
-echo "=================================-Script Premium-===========================" | tee -a log-install.txt
-echo "" | tee -a log-install.txt
-echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH              : 22"  | tee -a log-install.txt
-echo "   - OpenVPN              : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4             : 444, 777"  | tee -a log-install.txt
-echo "   - Dropbear             : 109, 143"  | tee -a log-install.txt
-echo "   - WS SSL               : 443"  | tee -a log-install.txt
-echo "   - WebSocket            : 80, 2095"  | tee -a log-install.txt
-echo "   - WebSocket Dropbear   : 8880, 2086"  | tee -a log-install.txt
-echo "   - WebSocket  Openssh   : 2052, 2082"  | tee -a log-install.txt
-echo "   - Squid Proxy          : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
-echo "   - Badvpn               : 7100, 7200, 7300"  | tee -a log-install.txt
-echo "   - Nginx                : 81"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone             : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
-echo "   - Fail2Ban             : [ON]"  | tee -a log-install.txt
-echo "   - Dflate               : [ON]"  | tee -a log-install.txt
-echo "   - IPtables             : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot          : [ON]"  | tee -a log-install.txt
-echo "   - IPv6                 : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On 08.00 GMT +7" | tee -a log-install.txt
-echo "   - Autobackup Data" | tee -a log-install.txt
-echo "   - Restore Data" | tee -a log-install.txt
-echo "   - Auto Delete Expired Account" | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
-echo "   - White Label" | tee -a log-install.txt
-echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   - Dev/Main            : Gapunya"  | tee -a log-install.txt
-echo "   - Telegram            : Gapunya"  | tee -a log-install.txt
-echo "   - Instagram           : Gapunya"  | tee -a log-install.txt
-echo "   - Whatsapp            : Gapunya"  | tee -a log-install.txt
-echo "   - Facebook            : Gapunya" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "------------------Script By Fauzan-Vpn-----------------" | tee -a log-install.txt
-echo ""
-echo " Reboot 10 Sec"
-sleep 10
-rm -f setup.sh
-reboot
+^?ELF^B^A^A^@^@^@^@^@^@^@^@^@^C^@�^@^A^@^@^@P^K^@^@^@^@^@^@@^@^@^@^@^@^@^@@$^@^@^@^@^@^@^@^@^@^@@^@8^>
+^@^@^@^Q^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@�^@^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@�^@^@^@^>
+^E^@^@^@^B^@�^@^@^@^@^@^@^@�/^@^@^@^@^@^@^C^D^@^@^@^@^@^@�^U^@^@^@^@^@^@�/^@^@^@^@^@^@^C^D^@^@^@^@^@^>
+^@^@^@^@^@^@^@^@^@^@^@H/^@^@^@^@^@^@^B^D^@^@^K^@^@^@^@^@^@^@^@^@^@^@P/^@^@^@^@^@^@^B^D^@^@^L^@^@^@^@^>
+^@^@^@^@^@^@^@^@^@^@^@`/^@^@^@^@^@^@^B^D^@^@^N^@^@^@^@^@^@^@^@^@^@^@h/^@^@^@^@^@^@^B^D^@^@^P^@^@^@^@^>
+^@^@�J�G�^@^Ag� ^]^XN�^C�=�^K^@�^B^@^@�B�G��^C^@��^C^_���^@�����@^@^@�^@^@^_��^C_�^H^@^@�^H�.�^B^@^@�>
+��^C^@��^C^H����^W^C^@^@�c@
+����^W^H^@^@�^_a
+9^H^@^@�^_e
+9^H^@^@�^_i
+9       ^@^@�(iJ9)iJ9�^C        *       ^@^@�)m
+�)^A
+�(^A^@9
+^@^@�HiJ9^H^E^@^Q�^C^H*Ii
+9^H^]^@^RH��5�^C_���^@��^O^@��^W^@��^O@��^G^@��^W@�^H^A^@q�^E^@T^K^@^@�hiJ9     ^@^@�)m
+�(^A^H�^H^A@9�O^@9�O@9
+^@^@�HaJ9^H^A^L^KHa
+9�^G@�liJ9�^W@��
+�^Z�}^N^[�^A
+k^L�l8HaJ9^H^A^L^KHa
+9HaJ9(^A^H�^H^A@9kiJ9+^A^K�h^A^@9�O@9JaJ9)^A
+�(^A^@9
+^@^@�HiJ9^H^E^@^Q�^C^H*Ii
+9^H^]^@^RH��5�^G@�^H^A^D��^G^@��^W@�^H^A^Dq�^W^@����^W��^@��^C_���^@��^O^@��^W^@��^O@��^G^@��^W@�^H^A>
+9(iJ9�^C^H*^H^@^@�^Hm
+��^C^H�J^A^K�J^A@9�O^@9�O@9^K^@^@�jeJ9J^A^L^Kje
+9jeJ9�^C
+*�^C^H�J^A^L�J^A@9,iJ9�^C^L*�^C^H��^A
+��^A^@9�O@9keJ9�^C^K*�^C^H�k^A^L�j^A^@9)iJ9
+ii8�O@9)^A
+^K�O^@9�O@9
+ii8�^G@�(^A@9^H^A
+J(^A^@9�^G@�^H^E^@��^G^@��^W@�^H^E^@q�^W^@����^W��^@��^C_��^C^E��{^R���^@���^D��^C^_��^C_��C^B�1���^H>
+k)^E^@q^A�)�|�����_��C@�^H^A    k^H^E^@^Q��^_�^C^@^@^T^H^@�^R��^_���_���        ��^K@��{¨�^C_��^C_���>
+^A��^_^@�       A^B��#^@�       i^B��'^@�       u^B��+^@�       �^B��/^@�       i^@��3^@�       �^B��>
+^@��^C^[�^H�^D���^[��C^_���^^���^�^H^A@��^C^\��^C\��^@^@�^@^@^@�^@^H^[�#����^C^\��^C\�(^A^@�^H^@^@�^H>
+*k^E^@^Q�C^]�)
+�(^A^@���]��^A^@4�^W@�^H�C9(^A^@4�^CZ���\��C���^C
+*k^E^@^Q�C^]�)
+�(^A^@��^W@�^HiB9(^A^@4�'@���\��C���^C
+*k^E^@^Q�C^]�)
+�(^A^@��^W@��^C^���\��C���^C^K*�^E^@^Q�C^]�J
+^K�I^A^@�^H�B9(^A^@4�/@���\��C���^C
+*k^E^@^Q�C^]�)
+�(^A^@���]�^H^E^@q�^@^@T��]��^O^@�^C^@^@^T�^C^_*�^O^@��^O@���^]���]��C_�^H^A    k�^A^@T��^������^C   >
+*k^E^@^Q�C^]�)
+�(^A^@����^W�#@���\��C��^H
+        �^_^A^@���\�6����#@���^_���_��{N���^C��^C_��C^A��{^D��^C^A���^_���^_��^C^_���_��^C_������^C_�>
+^@@�    ^@^@�)�^[�^H^@^@�^H�^[�J^A^@q^H^Q����^^�����^H^@@��^@^@4����^@^@@������^K^@�^D^@^@^T^H^@^@�^H>
+^@: ^@<null>^@^@^A^[^C;P^@^@^@  ^@^@^@����h^@^@^@0���|^@^@^@^P����^@^@^@^D����^@^@^@�����^@^@^@d����^>
+^@^@^@^@^@^@^@�^@^@^@^@^@^@^@^K^@^@^@^@^@^@^@^X^@^@^@^@^@^@^@^U^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^C^@^@^@>
+�!=�p�+�\<(^Oh��G^G}^���$6����9(�
+�y�{#z\^Z�]�o�ٳ�6C�g�^R��:T}���ݶ�4Z��٥U�)a^V�н�f��`��9CN|�
+����t^X��:R��~�2�� ���m^_^[��r#^Z�Xs-N^O�Z/�"����Aϸ�^F[b^]�PQq7W�1^X�����e�P�^L^R`]�
+�^X^N�H�^Y���`��^Lx2}Z�NPͨ��Q�П^^d��*��*x�^X�&��+3\���s��>j�$D�s^[�ˀ�+,�^Ea��9��hY^G�{�\ȕ�����R������F>
+tIr;�K��^Y��^��9�X^H��NTQ^P�0y�;W:����g���^QT�p^B���G�M^W^@�i^Q.������q��^V��k�^Ym��/^Ou}&v^_��N)^T^G>
