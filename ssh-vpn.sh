4b6f067a77940b79e6d181c1dca0cb9524d6f25f
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-11 09:15:10 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -293,7 +293,7 @@ echo "0 0 * * * root clear-log && reboot" >> /etc/crontab
 echo "0 0 * * * root xp" >> /etc/crontab
 
 #auto reboot server2
-echo "0 */08 * * * root clear-log && reboot" >> /etc/otomatis
+echo "0 */8 * * * root clear-log && reboot" >> /etc/otomatis
 echo "0 0 * * * root xp" >> /etc/otomatis
 
 # remove unnecessary files
