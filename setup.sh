26b0ee479a4018e0c8839d7db4f7fb866671cda3
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-20 06:26:04 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 if [ "${EUID}" -ne 0 ]; then
-  echo "You need to run this script as root"
-  exit 1
+		echo "You need to run this script as root"
+		exit 1
 fi
 if [ "$(systemd-detect-virt)" == "openvz" ]; then
-  echo "OpenVZ is not supported"
-  exit 1
+		echo "OpenVZ is not supported"
+		exit 1
 fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-if [ -f "/etc/v2ray/domain" ]; then
-echo "Script Already Installed"
+echo "Authentikasi pada server"
+IZIN=$( curl icanhazip.com | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permintaan Diterima...${NC}"
+else
+echo -e "${red}Permintaan Ditolak!${NC}";
+echo "Hanya untuk pengguna terdaftar"
+rm -f setup.sh
 exit 0
 fi
 mkdir /var/lib/premium-script;
 mkdir /etc/v2ray;
-echo "IP=" >> /var/lib/premium-script/ipvps.conf
+echo "Tolong masukan domain yang sudah dipointing agar v2ray service work"
+read -p "Hostname / Domain: " host
+echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo "$host" >> /etc/v2ray/domain
-wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/cf/cf.sh && chmod +x cf.sh && ./cf.sh
+
 #install ssh ovpn
 wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn.sh ./ssh-vpn.sh
 
