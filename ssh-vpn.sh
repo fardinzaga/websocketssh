a3407d283ced81528babafcf616ecdc5e324fabc
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-19 20:52:29 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# By Fauzanvpn
-#
+# Mod By Fauzanvpn
+# 
 # ==================================================
 
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
-MYIP=$(wget -qO- icanhazip.com);
+MYIP=$(wget -qO- ipinfo.io/ip);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 source /etc/os-release
@@ -15,10 +15,10 @@ ver=$VERSION_ID
 country=ID
 state=Indonesia
 locality=Indonesia
-organization=fauzanvpn.com
-organizationalunit=fauzanvpn.com
-commonname=fauzanvpn.com
-email=admin@fauzanvpn.com
+organization=Fauzanvpn.com
+organizationalunit=Fauzanvpn.com
+commonname=0.0.0.0
+email=Fauzanvpn.com
 
 # common password debian 
 wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/password/common-password-deb9"
@@ -62,12 +62,16 @@ systemctl start rc-local.service
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 
+# set repo
+sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
+apt install gnupg gnupg1 gnupg2 -y
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+
 #update
 apt update -y
 apt upgrade -y
 apt dist-upgrade -y
-apt-get remove --purge ufw firewalld -y
-apt-get remove --purge exim4 -y
 
 # install wget and curl
 apt -y install wget curl
@@ -79,13 +83,10 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 
 # install
-apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git
 echo "clear" >> .profile
-echo "echo ==========================" >> .profile
-echo "echo Selamat Datang Fauzanvpn !" >> .profile
-echo "echo Ketik menu untuk melihat list" >> .profile
-echo "echo VPSmu Terinstall AutoScript by Fauzanvpn" >> .profile
-echo "echo Terimakasih !" >> .profile
+echo "echo ================" >> .profile
+echo "echo Mod by Fauzanvpn" >> .profile
 
 # install webserver
 apt -y install nginx
@@ -142,22 +143,17 @@ apt -y install squid3
 wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/squid/squid3.conf"
 sed -i $MYIP2 /etc/squid/squid.conf
 
-# setting vnstat
-apt -y install vnstat
-/etc/init.d/vnstat restart
-apt -y install libsqlite3-dev
-wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
-tar zxvf vnstat-2.6.tar.gz
-cd vnstat-2.6
-./configure --prefix=/usr --sysconfdir=/etc && make && make install
-cd
-vnstat -u -i $NET
-sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
-chown vnstat:vnstat /var/lib/vnstat -R
+# setting dan install vnstat debian 9 64bit
+apt-get -y install vnstat
+systemctl start vnstat
 systemctl enable vnstat
-/etc/init.d/vnstat restart
-rm -f /root/vnstat-2.6.tar.gz
-rm -rf /root/vnstat-2.6
+chkconfig vnstat on
+chown -R vnstat:vnstat /var/lib/vnstat
+
+# install webmin
+apt install webmin -y
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+/etc/init.d/webmin restart
 
 # install stunnel
 apt install stunnel4 -y
@@ -167,31 +163,31 @@ client = no
 socket = a:SO_REUSEADDR=1
 socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
-
 [ws-stunnel]
 accept = 443
 connect = 127.0.0.1:97
-
 [dropbear]
 accept = 444
 connect = 127.0.0.1:44
-
 [dropbear]
 accept = 777
 connect = 127.0.0.1:22
-
 END
 
-# make a certificate
+echo "=================  membuat Sertifikat OpenSSL ======================"
+echo "========================================================="
+#membuat sertifikat
+cd /etc/stunnel/
 openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
 -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
-
+cd
 # konfigurasi stunnel
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+cd
 /etc/init.d/stunnel4 restart
-
+cd
 #install sslh
 cd
 apt-get install sslh -y
@@ -214,7 +210,6 @@ cat > /etc/systemd/system/edu-proxy.service << END
 Description=Python Edu Proxy By Fauzanvpn
 Documentation=admin@Fauzanvpn
 After=network.target nss-lookup.target
-
 [Service]
 Type=simple
 User=root
@@ -223,7 +218,6 @@ AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 80
 Restart=on-failure
-
 [Install]
 WantedBy=multi-user.target
 END
@@ -268,6 +262,10 @@ echo; echo 'Installation has completed.'
 echo 'Config file is at /usr/local/ddos/ddos.conf'
 echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 
+# xml parser
+cd
+apt install -y libxml-parser-perl
+
 # Custom Banner SSH
 echo "================  Banner ======================"
 wget -O /etc/issue.net "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/banner/banner-custom.conf"
@@ -277,9 +275,22 @@ chmod +x /etc/issue.net
 echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
+#install bbr dan optimasi kernel
+wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/bbr/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+wget https://raw.githubusercontent.com/fardinzaga/installerssh/master/bbr/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+
 # blockir torrent
-apt install iptables-persistent -y
-wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/security/torrent && chmod +x torrent && ./torrent
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
 iptables-save > /etc/iptables.up.rules
 iptables-restore -t < /etc/iptables.up.rules
 netfilter-persistent save
@@ -305,6 +316,10 @@ wget -O ceklim "https://raw.githubusercontent.com/fardinzaga/websocketssh/master
 wget -O tendang "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/menu/tendang.sh"
 wget -O wbmn "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/menu/webmin.sh"
 wget -O kernel-updt "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/menu/karnel-update.sh"
+
+echo "0 8 * * * root clear-log && reboot" >> /etc/crontab
+echo "0 0 * * * root xp" >> /etc/crontab
+
 chmod +x add-host 
 chmod +x menu
 chmod +x usernew
@@ -324,25 +339,15 @@ chmod +x renew
 chmod +x wbmn
 chmod +x kernel-updt
 
-#install websocker SSH dan Dropbear
-wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/websocket/install-ws.sh && chmod +x install-ws.sh && ./install-ws.sh
-
-# Delete Acount SSH Expired
-echo "================  Auto deleted Account Expired ======================"
-wget -O /usr/local/bin/userdelexpired "https://raw.githubusercontent.com/fardinzaga/websocketssh/master/userdelexpired" && chmod +x /usr/local/bin/userdelexpired
-
-echo "0 8 * * * root clear-log && reboot" >> /etc/crontab
-echo "0 0 * * * root xp" >> /etc/crontab
-
 # remove unnecessary files
-cd
-apt autoclean -y
+apt -y autoclean
 apt -y remove --purge unscd
 apt-get -y --purge remove samba*;
 apt-get -y --purge remove apache2*;
 apt-get -y --purge remove bind9*;
 apt-get -y remove sendmail*
-apt autoremove -y
+apt -y autoremove
+
 # finishing
 cd
 chown -R www-data:www-data /home/vps/public_html
@@ -352,18 +357,18 @@ chown -R www-data:www-data /home/vps/public_html
 /etc/init.d/ssh restart
 /etc/init.d/dropbear restart
 /etc/init.d/fail2ban restart
+/etc/init.d/webmin restart
 /etc/init.d/stunnel4 restart
 /etc/init.d/vnstat restart
 /etc/init.d/squid restart
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+
 history -c
-echo "unset Fauzanvpn" >> /etc/profile
+echo "unset HISTFILE" >> /etc/profile
 
 cd
-rm -f /root/key.pem
-rm -f /root/cert.pem
 rm -f /root/ssh-vpn.sh
 
 # finihsing
