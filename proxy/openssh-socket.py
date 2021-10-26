a868ae4cfed8ecd24ab5c6ae50f57628e0320680
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-27 04:51:03 +0700

    Update openssh-socket.py

--- proxy/openssh-socket.py
+++ proxy/openssh-socket.py
@@ -4,17 +4,18 @@ import socket, threading, thread, select, signal, sys, time, getopt
 # Listen
 LISTENING_ADDR = '0.0.0.0'
 if sys.argv[1:]:
-  LISTENING_PORT = sys.argv[1]
+	LISTENING_PORT = sys.argv[1]
 else:
-  LISTENING_PORT = 2082
+	LISTENING_PORT = 2082
+
 # Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:88'
-RESPONSE = 'HTTP/1.1 101 <b><font color="red">SETUP BY FAUZAN-VPN<b></font>\r\nContent-Length: 104857600000\r\n\r\n'
+DEFAULT_HOST = '127.0.0.1:1194'
+RESPONSE = 'HTTP/1.1 101 <b><font color="red">SETUP BY FAUZAN-VPN</b></font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
@@ -265,3 +266,4 @@ def main(host=LISTENING_ADDR, port=LISTENING_PORT):
 #######    parse_args(sys.argv[1:])
 if __name__ == '__main__':
     main()
+
