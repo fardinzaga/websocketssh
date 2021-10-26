bafadaf2199a77fcb822e54141d4e80dc86b84ca
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-10-27 05:53:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -50,7 +50,7 @@ echo "   - Dropbear             : 109, 143"  | tee -a log-install.txt
 echo "   - Ws SSL/TLS           : 443"  | tee -a log-install.txt
 echo "   - WebSocket            : 80, 2095"  | tee -a log-install.txt
 echo "   - WsDropbear           : 8880, 2086"  | tee -a log-install.txt
-echo "   - WsOpenssh            : 2052"  | tee -a log-install.txt
+echo "   - WsOpenssh            : 2052, 2053"  | tee -a log-install.txt
 echo "   - WsOvpn               : 2082"  | tee -a log-install.txt
 echo "   - Squid Proxy          : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn               : 7100, 7200, 7300"  | tee -a log-install.txt
