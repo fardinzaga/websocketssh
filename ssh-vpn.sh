8762c348898c6bf42ba3610d06d2cb9bdfe1c40f
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-11-02 19:51:01 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -241,6 +241,22 @@ echo -e "Fauzanvpn"
 #OpenVPN
 wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/vpn/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
 
+if [ ! -e /home/vps/public_html/TCP.ovpn ]; then
+cp /etc/openvpn/client-tcp-1194.ovpn /home/vps/public_html/TCP.ovpn
+cp /etc/openvpn/client-udp-2200.ovpn /home/vps/public_html/UDP.ovpn
+cp /etc/openvpn/client-tcp-ssl.ovpn /home/vps/public_html/SSL.ovpn
+
+mkdir /root/OpenVPN
+cp -r /etc/openvpn/client-tcp-ssl.ovpn OpenVPN/SSL.ovpn
+cp -r /etc/openvpn/client-udp-2200.ovpn OpenVPN/UDP.ovpn
+cp -r /etc/openvpn/client-tcp-1194.ovpn OpenVPN/TCP.ovpn
+cd /root
+zip -r openvpn.zip OpenVPN > /dev/null 2>&1
+cp -r /root/openvpn.zip /home/vps/public_html/ALL.zip
+rm -rf /root/OpenVPN
+rm -f /root/openvpn.zip
+fi
+
 # install fail2ban
 apt -y install fail2ban
 
