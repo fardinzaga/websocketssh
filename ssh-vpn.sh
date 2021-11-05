05e80047ecc6793f4cf2a77700ea23928257a839
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-11-06 04:44:49 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -85,10 +85,10 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 # install
 apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
 echo "clear" >> .profile
-echo "echo ==========================" >> .profile
+echo "echo =============================" >> .profile
 echo "echo Selamat Datang Fauzanvpn !" >> .profile
 echo "echo Ketik menu untuk melihat list" >> .profile
-echo "echo ==========================" >> .profile
+echo "echo =============================" >> .profile
 
 # install webserver
 apt -y install nginx
