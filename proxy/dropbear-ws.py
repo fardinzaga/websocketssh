5448c50e1bcca318aa989d1cb345990aaeee678d
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-11-09 18:05:57 +0700

    Update dropbear-ws.py

--- proxy/dropbear-ws.py
+++ proxy/dropbear-ws.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
@@ -5,16 +6,15 @@ LISTENING_ADDR = '0.0.0.0'
 if sys.argv[1:]:
   LISTENING_PORT = sys.argv[1]
 else:
-  LISTENING_PORT = 2086  
-#Pass
+  LISTENING_PORT = 2086
+# Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:143'
-RESPONSE = 'HTTP/1.1 101 <b><font color="red">SETUP BY FAUZAN-VPN</b></font>\r\n\r\n'
-#RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
+RESPONSE = 'HTTP/1.1 101 <b><font color="red">SETUP BY FAUZAN-VPN</b></font>\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
@@ -170,7 +170,7 @@ class ConnectionHandler(threading.Thread):
             host = host[:i]
         else:
             if self.method=='CONNECT':
-                port = 443
+                port = 445
             else:
                 port = sys.argv[1]
 
