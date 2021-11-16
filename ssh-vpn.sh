938e9cc9c63ceba15f62fbc7a4f3d8c7bef7a8f2
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-11-16 18:15:04 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -194,12 +194,12 @@ accept = 443
 connect = 127.0.0.1:109
 
 [dropbear1]
-accept = 222
-connect = 127.0.0.1:22
+accept = 444
+connect = 127.0.0.1:44
 
 [dropbear2]
-accept = 777
-connect = 127.0.0.1:44
+accept = 222
+connect = 127.0.0.1:22
 
 [ws-stunnel]
 accept = 2096
