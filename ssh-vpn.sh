c9bd9a1b980bda5f6d7c0c6698bf93fa03f1eff7
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-11-17 10:57:50 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -189,10 +189,6 @@ socket = a:SO_REUSEADDR=1
 socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
 
-[dropbear]
-accept = 443
-connect = 127.0.0.1:109
-
 [dropbear2]
 accept = 444
 connect = 127.0.0.1:44
