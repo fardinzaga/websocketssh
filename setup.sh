71c4c5a554261f019c6386c5979827bf379445a1
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-11-25 00:37:18 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 if [ "${EUID}" -ne 0 ]; then
-echo "You need to run this script as root"
-exit 1
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
 fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
-IZIN=$( curl http://ipinfo.io/ip | grep $MYIP )
+echo "Authentikasi pada server"
+IZIN=$( curl icanhazip.com | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permintaan Diterima...${NC}"
 else
 echo -e "${red}Permintaan Ditolak!${NC}";
 echo "Hanya untuk pengguna terdaftar"
+rm -f setup.sh
+exit 0
 fi
-mkdir /etc/v2ray
-mkdir /var/lib/crot-script;
-clear
-echo "Masukkan Domain Anda, Jika Anda Tidak Memiliki Domain Klik Enter"
-echo "Ketikkan Perintah addhost setelah proses instalasi Script Selesai"
+mkdir /var/lib/premium-script;
+mkdir /etc/v2ray;
+echo "Tolong masukan domain yang sudah dipointing agar v2ray service work"
 read -p "Hostname / Domain: " host
-echo "IP=$host" >> /var/lib/crot-script/ipvps.conf
+echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo "$host" >> /etc/v2ray/domain
 
 #install ssh ovpn
