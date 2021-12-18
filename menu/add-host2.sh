524e21c4eaa32f916780cc29d39c0a7ce7eefe3f
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-12-18 10:11:21 +0700

    Update add-host2.sh

--- menu/add-host2.sh
+++ menu/add-host2.sh
@@ -1,48 +1,29 @@
 #!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-apt install jq curl -y
-rm -f /root/domain
-rm -f /etc/v2ray/domain
-clear
-
-DOMAIN=posweqo.tk
-#read -rp "Masukkan Domain: " -e DOMAIN
-#echo ""
-#echo "Domain: ${DOMAIN}" 
-#echo ""
-read -rp "Masukkan Subdomain: " -e sub
-SUB_DOMAIN=${sub}.${DOMAIN}
-CF_ID=henzrolin@gmailwe.com
-CF_KEY=122794653c0c895e171f2e0a090adbeeca5e9
-set -euo pipefail
-IP=$(wget -qO- ipinfo.io/ip);
-echo "Pointing DNS Untuk Domain ${SUB_DOMAIN}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+echo "Authentikasi pada server"
+IZIN=$( curl icanhazip.com | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permintaan Diterima...${NC}"
+else
+echo -e "${red}Permintaan Ditolak!${NC}";
+echo "Hanya untuk pengguna terdaftar"
+rm -f setup.sh
+exit 0
 fi
-
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
-echo "Host : $SUB_DOMAIN"
-echo $SUB_DOMAIN > /root/domain
-echo $SUB_DOMAIN > /etc/v2ray/domain
+mkdir /var/lib/premium-script;
+mkdir /etc/v2ray;
+echo "Tolong masukan domain yang sudah dipointing agar v2ray service work"
+read -p "Hostname / Domain: " host
+echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+echo "$host" >> /etc/v2ray/domain
