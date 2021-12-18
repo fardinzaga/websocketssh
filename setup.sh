6a6f8830ddca8211607d7beb4fae89f449854584
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-12-18 17:57:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,18 +11,23 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-clear
-if [ -f "/etc/v2ray/domain" ]; then
-echo "Script Already Installed"
+echo "Authentikasi pada server"
+IZIN=$( curl icanhazip.com | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permintaan Diterima...${NC}"
+else
+echo -e "${red}Permintaan Ditolak!${NC}";
+echo "Hanya untuk pengguna terdaftar"
+rm -f setup.sh
 exit 0
 fi
 mkdir /var/lib/premium-script;
 mkdir /etc/v2ray;
+echo "Tolong masukan domain yang sudah dipointing agar v2ray service work"
+read -p "Hostname / Domain: " host
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo "$host" >> /etc/v2ray/domain
 
-#install cf
-wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/cf/cf.sh && chmod +x cf.sh && screen -S cf.sh ./cf.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn.sh ./ssh-vpn.sh
 
