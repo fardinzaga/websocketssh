707ba24bc28c42b6d4f8772ce4d426f253ec9956
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-12-18 10:04:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,6 +24,7 @@ wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/cf/cf.sh &
 #install ssh ovpn
 wget https://raw.githubusercontent.com/fardinzaga/websocketssh/master/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn.sh ./ssh-vpn.sh
 
+rm -f /root/cf.sh
 rm -f /root/ssh-vpn.sh
 history -c
 echo "1.2" > /home/ver
