56a64753c9dc69a1226ad9106ea1be26009c1070
fardinzaga <87352137+fardinzaga@users.noreply.github.com>
2021-12-25 09:19:35 +0700

    Update install-ws.sh

--- websocket/install-ws.sh
+++ websocket/install-ws.sh
@@ -8,9 +8,9 @@ cd
 #wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/openssh-socket.py
 wget -O /usr/local/bin/ws-udin https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/udin-ws.py
 wget -O /usr/local/bin/ws-maulana https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/maulana-ws.py
-wget -O /usr/local/bin/ws-zahara https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/zahara-ws.py
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/dropbear-ws.py
 wget -O /usr/local/bin/ws-fauzanvpn https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/fauzanvpn-ws.py
+#wget -O /usr/local/bin/ws-zahara https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/zahara-ws.py
 #wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/ws-ovpn.py && chmod +x /usr/local/bin/ws-ovpn.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/ws-stunnel.py && chmod +x /usr/local/bin/ws-stunnel.py
 
@@ -18,9 +18,9 @@ wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/fardinzaga/w
 #chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-udin
 chmod +x /usr/local/bin/ws-maulana
-chmod +x /usr/local/bin/ws-zahara
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-fauzanvpn
+#chmod +x /usr/local/bin/ws-zahara
 #chmod +x /usr/local/bin/ws-ovpn
 chmod +x /usr/local/bin/ws-stunnel
 
@@ -33,14 +33,14 @@ wget -O /etc/systemd/system/ws-udin.service https://raw.githubusercontent.com/fa
 #System OpenSSH Websocket-SSH Python
 wget -O /etc/systemd/system/ws-maulana.service https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/service.wsmaulana && chmod +x /etc/systemd/system/ws-maulana.service
 
-#System OpenSSH Websocket-SSH Python
-wget -O /etc/systemd/system/ws-zahara.service https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/service.wszahara && chmod +x /etc/systemd/system/ws-zahara.service
-
 #System Dropbear Websocket-SSH Python
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System OpenSSH Websocket-SSH Python
 wget -O /etc/systemd/system/ws-fauzanvpn.service https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/service-wsfauzanvpn && chmod +x /etc/systemd/system/ws-fauzanvpn.service
+
+#System OpenSSH Websocket-SSH Python
+#wget -O /etc/systemd/system/ws-zahara.service https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/service.wszahara && chmod +x /etc/systemd/system/ws-zahara.service
 
 #System Websocket-OpenVPN Python
 #wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/fardinzaga/websocketssh/master/proxy/service.wsovpn && chmod +x /etc/systemd/system/ws-ovpn.service
@@ -67,11 +67,6 @@ systemctl enable ws-maulana.service
 systemctl start ws-maulana.service
 systemctl restart ws-maulana.service
 
-#Enable & Start & Restart ws-zahara service
-systemctl enable ws-zahara.service
-systemctl start ws-zahara.service
-systemctl restart ws-zahara.service
-
 #Enable & Start & Restart ws-dropbear service
 systemctl enable ws-dropbear.service
 systemctl start ws-dropbear.service
@@ -81,6 +76,11 @@ systemctl restart ws-dropbear.service
 systemctl enable ws-fauzanvpn.service
 systemctl start ws-fauzanvpn.service
 systemctl restart ws-fauzanvpn.service
+
+#Enable & Start & Restart ws-zahara service
+#systemctl enable ws-zahara.service
+#systemctl start ws-zahara.service
+#systemctl restart ws-zahara.service
 
 #Enable & Start ws-ovpn service
 #systemctl enable ws-ovpn.service
